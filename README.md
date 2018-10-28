305b7b40bd012bbad61c615a34db1ed5bd3a80b7
Anish Basu <anish@drexel.edu>
2018-10-28 15:02:53 -0400

    Added setup and requirements

---
+++ README.md
@@ -0,0 +1,4 @@
+EtherFx-Core
+===
+
+Repository for shared code between the etherfx client and etherfx server.
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='etherfx-core',
+    version='0.1-RC',
+    packages=['src'],
+    license='GNU General Public License v3.0',
+    long_description=open('README.md').read()
+)
